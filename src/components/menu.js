2cda3b179ea9b015a055d4b78bc19833cf0e20c3
colditz <patric.crvz@gmail.com>
2022-06-18 03:06:25 +0800

    updated resume

--- src/components/menu.js
+++ src/components/menu.js
@@ -262,7 +262,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/portfolio/resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
